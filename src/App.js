15b644074460b8ea3592aa7c4d2ef4df0aa1fdc8
crileyy <criley0007@gmail.com>
2019-04-03 19:10:41 -0700

    Added tabs

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 import { Header } from './components/Header';
 import { ExperienceList } from './components/ExperienceList'
 import { ProjectList } from './components/ProjectList';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "./components/react-tabs.css";
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
       <div className="App-body">
-        <ExperienceList />
-        <ProjectList />
+        <Tabs>
+          <TabList>
+            <Tab>Experience</Tab>
+            <Tab>Projects</Tab>
+            <Tab>Music</Tab>
+          </TabList>
+
+          <TabPanel>
+            <ExperienceList />
+          </TabPanel>
+          <TabPanel>
+            <ProjectList />
+          </TabPanel>
+          <TabPanel>
+            <span>music!</span>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
